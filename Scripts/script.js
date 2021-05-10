475eb7844749445c31ad08fa6dcd4d16457f1fca
Renan Paixão <renanlemos21@gmail.com>
2021-05-10 12:57:14 -0300

    feat(inputs-textarea): placeholders agora somem

--- Scripts/script.js
+++ Scripts/script.js
@@ -16,3 +16,44 @@ const Pagina = {
 		document.getElementById('contato').style.display = 'none';
 	},
 };
+
+const Utils = {
+	escondePlaceholder() {
+		var input = document.getElementsByTagName('input');
+
+		document.getElementById('textareaContato').addEventListener('click', () => {
+			document.getElementById('textareaContato').placeholder = '';
+		});
+
+		Array.from(input).forEach((element) => {
+			element.addEventListener('click', function () {
+				element.placeholder = '';
+			});
+		});
+	},
+	mostrarPlaceholder() {
+		var input = document.getElementsByTagName('input');
+		document.getElementById('textareaContato').addEventListener('focusout', () => {
+			document.getElementById('textareaContato').placeholder = 'Mensagem';
+		});
+
+		input = Array.from(input);
+		input[0].addEventListener('focusout', () => {
+			input[0].placeholder = 'Nome Completo';
+		});
+		input[1].addEventListener('focusout', () => {
+			input[1].placeholder = 'Email';
+		});
+	},
+};
+
+function init() {
+	try {
+		Utils.escondePlaceholder();
+		Utils.mostrarPlaceholder();
+	} catch (error) {
+		alert(error.message);
+	}
+}
+
+init();
